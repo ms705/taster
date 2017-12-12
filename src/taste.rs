ms705/taster

commit 9afebd039f2a762f1c51136a597a3786dd3f5ed4

Turn history hashmap into modular trait object
This allows us to use different backends for history storage. Adds a
simple in-memory backend that basically does what the history hash table
did before, modulo an extra level of indirection, as it keeps history
for all commits, not just the head commit of each branch.

src/taste.rs
@@ -1,5 +1,5 @@
 use config::{parse_config, Benchmark, Config};
-use history::History;
+use history::HistoryDB;
 use Commit;
 use git2;
 use Push;
@@ -193,7 +193,7 @@ fn build(workdir: &str) -> Output {
 
 pub fn taste_commit(
     ws: &Workspace,
-    history: &mut History,
+    history: &mut HistoryDB,
     push: &Push,
     commit: &Commit,
     def_improvement_threshold: f64,
@@ -287,29 +287,33 @@ pub fn taste_commit(
         },
     };
 
-    let bench_results =
-        match branch {
-            Some(ref branch) => {
-                let branch_history = history.mut_branch_head(&branch);
-                cfg.benchmarks
-                .iter()
-                .map(|b| {
-                    let (status, res) =
-                        benchmark(&ws.path, &cfg, b, commit.id, branch_history.get(&b.name),
-                                  timeout);
-                    branch_history.insert(b.name.clone(), res.clone());
-                    (b.clone(), status, res)
-                })
-                .collect::<Vec<(Benchmark, ExitStatus, HashMap<String, BenchmarkResult<f64>>)>>()
-            }
-            None => cfg.benchmarks
-                .iter()
-                .map(|b| {
-                    let (status, res) = benchmark(&ws.path, &cfg, b, commit.id, None, timeout);
-                    (b.clone(), status, res)
-                })
-                .collect::<Vec<(Benchmark, ExitStatus, HashMap<String, BenchmarkResult<f64>>)>>(),
-        };
+    let bench_results = match branch {
+        Some(ref branch) => cfg.benchmarks
+            .iter()
+            .map(|b| {
+                let head_history = history.get_commit(branch, None).unwrap();
+                let (status, res) = benchmark(
+                    &ws.path,
+                    &cfg,
+                    b,
+                    commit.id,
+                    head_history.get(&b.name),
+                    timeout,
+                );
+                let mut entry = HashMap::new();
+                entry.insert(b.name.clone(), res.clone());
+                history.put(branch, commit.id, entry).unwrap();
+                (b.clone(), status, res)
+            })
+            .collect::<Vec<(Benchmark, ExitStatus, HashMap<String, BenchmarkResult<f64>>)>>(),
+        None => cfg.benchmarks
+            .iter()
+            .map(|b| {
+                let (status, res) = benchmark(&ws.path, &cfg, b, commit.id, None, timeout);
+                (b.clone(), status, res)
+            })
+            .collect::<Vec<(Benchmark, ExitStatus, HashMap<String, BenchmarkResult<f64>>)>>(),
+    };
     let bench_success = bench_results.iter().all(|x| x.1.success());
 
     Ok((

src/history.rs
@@ -1,21 +1,0 @@
-use std::collections::HashMap;
-
-use taste::BenchmarkResult;
-
-// (benchmark, (metric, result))
-// TODO(malte): proper types
-type BranchHistoryEntry = HashMap<String, HashMap<String, BenchmarkResult<f64>>>;
-
-pub struct History {
-    db: HashMap<String, BranchHistoryEntry>,
-}
-
-impl History {
-    pub fn new() -> Self {
-        History { db: HashMap::new() }
-    }
-
-    pub fn mut_branch_head(&mut self, branch: &str) -> &mut BranchHistoryEntry {
-        self.db.entry(branch.to_string()).or_insert(HashMap::new())
-    }
-}

src/history/in_memory.rs
@@ -1,0 +1,64 @@
+use std::collections::HashMap;
+
+use git2;
+
+use super::{BranchHistoryEntry, HistoryDB};
+
+pub struct InMemoryHistoryDB {
+    db: HashMap<String, HashMap<git2::Oid, BranchHistoryEntry>>,
+    heads: HashMap<String, git2::Oid>,
+}
+
+impl InMemoryHistoryDB {
+    pub fn new() -> Self {
+        InMemoryHistoryDB {
+            db: HashMap::new(),
+            heads: HashMap::new(),
+        }
+    }
+}
+
+impl HistoryDB for InMemoryHistoryDB {
+    fn put(
+        &mut self,
+        branch: &str,
+        commit: git2::Oid,
+        entry: BranchHistoryEntry,
+    ) -> Result<(), String> {
+        let bh = self.db.entry(branch.to_string()).or_insert(HashMap::new());
+        bh.insert(commit, entry);
+        self.heads.insert(branch.to_owned(), commit);
+
+        Ok(())
+    }
+
+    fn get_commit(
+        &self,
+        branch: &str,
+        commit: Option<&git2::Oid>,
+    ) -> Result<BranchHistoryEntry, String> {
+        let commit = match commit {
+            None => {
+                // use HEAD
+                self.heads.get(branch).unwrap()
+            }
+            Some(c) => c,
+        };
+
+        match self.db.get(branch) {
+            None => Err(format!("no history found for branch {}", branch)),
+            Some(ref bh) => match bh.get(commit) {
+                None => Err(format!(
+                    "commit {} not found in history for branch {}!",
+                    commit,
+                    branch
+                )),
+                Some(ch) => Ok(ch.clone()),
+            },
+        }
+    }
+
+    fn get_branch_history(&self, _branch: &str) -> Result<Vec<BranchHistoryEntry>, String> {
+        unimplemented!();
+    }
+}

src/history/mod.rs
@@ -1,0 +1,31 @@
+use std::collections::HashMap;
+
+use git2;
+
+use taste::BenchmarkResult;
+
+pub mod in_memory;
+#[cfg(soup)]
+pub mod soup;
+
+// (benchmark, (metric, result))
+// TODO(malte): proper types
+type BranchHistoryEntry = HashMap<String, HashMap<String, BenchmarkResult<f64>>>;
+
+// TODO(malte): should not need to be `Send` once we have a tasting queue
+pub trait HistoryDB: Send {
+    fn put(
+        &mut self,
+        branch: &str,
+        commit: git2::Oid,
+        entry: BranchHistoryEntry,
+    ) -> Result<(), String>;
+
+    fn get_commit(
+        &self,
+        branch: &str,
+        commit: Option<&git2::Oid>,
+    ) -> Result<BranchHistoryEntry, String>;
+
+    fn get_branch_history(&self, branch: &str) -> Result<Vec<BranchHistoryEntry>, String>;
+}

src/taster.rs
@@ -3,7 +3,7 @@ use common::{self, Commit, Push};
 use config::Config;
 use email;
 use github;
-use history::History;
+use history::{self, HistoryDB};
 use repo::{self, Workspace};
 use slack;
 use taste;
@@ -16,7 +16,7 @@ pub struct Taster {
     log: slog::Logger,
 
     ws: Workspace,
-    history: History,
+    history: Box<HistoryDB>,
 
     en: Option<email::EmailNotifier>,
     gn: Option<github::GithubNotifier>,
@@ -55,7 +55,7 @@ impl Taster {
             log: common::new_logger(),
 
             ws: repo::Workspace::new(&repo, workdir),
-            history: History::new(),
+            history: Box::new(history::in_memory::InMemoryHistoryDB::new()),
 
             gn: gn,
             sn: sn,
@@ -92,7 +92,7 @@ impl Taster {
             };
             let res = taste::taste_commit(
                 &self.ws,
-                &mut self.history,
+                self.history.as_mut(),
                 &push,
                 &push.head_commit,
                 self.args.improvement_threshold,
@@ -147,7 +147,7 @@ impl Taster {
         self.ws.fetch().unwrap();
         let head_res = taste::taste_commit(
             &self.ws,
-            &mut self.history,
+            self.history.as_mut(),
             &push,
             &push.head_commit,
             self.args.improvement_threshold,
@@ -174,7 +174,7 @@ impl Taster {
                         // taste
                         let res = taste::taste_commit(
                             &self.ws,
-                            &mut self.history,
+                            self.history.as_mut(),
                             &push,
                             &cur_c,
                             self.args.improvement_threshold,

commit 570f257463204ae23ed613e74bd47049a8146b3c

Correctly run bootstrap when there is no history
Regression introduced in 9afebd039f2a762f1c51136a597a3786dd3f5ed4:
when bootstrapping, there is no history; the old API automatically
created and filled an empty `HashMap` in that case, but since the API
change to `get_commit`, we no longer do so.

src/taste.rs
@@ -291,15 +291,12 @@ pub fn taste_commit(
         Some(ref branch) => cfg.benchmarks
             .iter()
             .map(|b| {
-                let head_history = history.get_commit(branch, None).unwrap();
-                let (status, res) = benchmark(
-                    &ws.path,
-                    &cfg,
-                    b,
-                    commit.id,
-                    head_history.get(&b.name),
-                    timeout,
-                );
+                let head_history = match history.get_commit(branch, None) {
+                    Ok(h) => Some(h.get(&b.name).unwrap().clone()),
+                    Err(_) => None,
+                };
+                let (status, res) =
+                    benchmark(&ws.path, &cfg, b, commit.id, head_history.as_ref(), timeout);
                 let mut entry = HashMap::new();
                 entry.insert(b.name.clone(), res.clone());
                 history.put(branch, commit.id, entry).unwrap();